77b763f71fc12de4bb0c87b6e40d8ef64108afd3
mjbaucas <mjbaucas@gmail.com>
2019-01-07 22:09:55 -0500

    Added signing and verifying messages

--- node_class.py
+++ node_class.py
@@ -4,6 +4,11 @@ import math
 import hashlib
 from custom_cipher import CustomCipher
 
+# Temporary Ledger initialization
+from Crypto.PublicKey import RSA
+from aes_cipher import AESCipher
+
+
 class Node:
     def __init__(self, serial_key, key_file):
         self.cipher = CustomCipher()
@@ -14,9 +19,16 @@ class Node:
         
         #print public_key
         #print secret_key
+        
+        # Temporary Ledger initialization
+        aes_cipher = AESCipher()
+        
+        dev1_serial = "Device0001"
+        dev1_serial_hash = hashlib.sha256(dev1_serial.encode()).digest()
+        dev1_pub = aes_cipher.encrypt(RSA.importKey(open("device1-public.pem", "rb")).exportKey('PEM'), dev1_serial_hash)
+        
         self.ledger = {
-            "NoDe0001": public_key, 
-            "Device0002": public_key
+            dev1_serial: dev1_pub
         }
         # self.blockchain = []
     
@@ -29,44 +41,37 @@ class Node:
     def decrypt(self, message):
         return self.cipher.decrypt(message, self.key)
         
-    def add_to_ledger(self, users):
-        if isinstance(users, dict):
-            for key, item in users.items():
-                self.ledger[key] = item
+    def sign(self, data):
+        signature = self.cipher.sign(self.key, data)
+        return signature
+        
+    def verify(self, data, signature):
+        return self.cipher.verify(self.public_key, self.secret_key, data, signature)
 
     def process_message(self, message):
         print message
         
 class Device:
-    def __init__(self, serial_key, id):
-        self.cipher = RSACipher()
-        private_key, public_key, key = self.cipher.gen_keys(serial_key)
-        self.private_key = private_key
+    def __init__(self, serial_key, key_file):
+        self.cipher = CustomCipher()
+        key, public_key, secret_key = self.cipher.init_keys(serial_key, key_file)
+        self.secret_key = secret_key
         self.public_key = public_key
         self.key = key
         # self.blockchain = []
     
-    def is_granted_access(self):
-        return self.node_key != ""
-    
-    def set_node_key(self, pub_key):
-        self.node_key = pub_key
-
-    def encrypt(self, message, key):
-        if key:
-            return self.cipher.encrypt(message, key)
-        else:     
-            return self.cipher.encrypt(message, self.public_key)
+    def encrypt(self, serial, message):
+        if serial in self.ledger:
+            public_key = self.ledger[serial]
+            secret_key = hashlib.sha256(serial.encode()).digest()
+            return self.cipher.encrypt(public_key, secret_key, message)
         
-    def decrypt(self, message, key):
-        if key:
-            return self.cipher.decrypt(message, key)
-        else:        
-            return self.cipher.decrypt(message,self.private_key)
+    def decrypt(self, message):
+        return self.cipher.decrypt(message, self.key)
     
-    def sign(self, message, key):
-        if key:
-            return self.cipher.sign(message, key)
-        else:        
-            return self.cipher.sign(message,self.private_key)
+    def sign(self, data):
+        signature = self.cipher.sign(self.key, data)
+        return signature
         
+    def verify(self, data, signature):
+        return self.cipher.verify(self.public_key, self.secret_key, data, signature)
--- server.py
+++ server.py
@@ -5,13 +5,18 @@ from node_class import Node, Device
 
 node_private_key = "NoDe0001"
 server_node = Node("NoDe0001", "server-private.pem")
+device_one = Device("Device0001", "device1-private.pem")
 
 bind_ip = '192.168.0.103'
 bind_port = 9999
 
-mess = server_node.encrypt("NoDe0001", "Hello World")
-print mess
-print server_node.decrypt(mess)
+mess = server_node.encrypt("Device0001", "Hello World")
+#print mess
+#if mess is not None:
+#    print device_one.decrypt(mess)
+    
+sign = device_one.sign("Message")
+print device_one.verify("Message1212", sign)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
